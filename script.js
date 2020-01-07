ae02b56b052f7d73133fb13c2f3213cb056f90ef
Joel D'Autilio <joeldautilio@gmail.com>
2020-01-07 14:28:25 -0300

    Added functions for basic math operators

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    return (operator == 'add') ? add(a, b) :
+           (operator == 'subtract') ? subtract(a, b) :
+           (operator == 'multiply') ? multiply(a, b) :
+           (operator == 'divide') ? divide(a, b) :
+           0;
+    
+}
